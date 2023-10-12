dbec7de91d3b7989ac4ceda4834870c060219196
Mike Ziwisky <mike.ziwisky@lumio.com>
2023-10-12 00:06:29 -0700

    got different number of things rendering at different zoom depths
    took 5evar to figure out how to incorporate the datum into a loop like
    this.  turns out .append can take a data function just like all these
    other d3 methods, so that's handy

--- src/main.js
+++ src/main.js
@@ -26,18 +26,19 @@ import {tile as d3tile} from 'd3-tile'
 
 const width = 600
 const height = 600
+const MAX_DEPTH = 14 // empirically found, when scaleExtent is 1<<22
 
 const map = function() {
   const svg = d3.create("svg")
       .attr("viewBox", [0, 0, width, height]);
 
   const tiler = d3tile()
-      .extent([[0, 0], [width, height]]);
+    .extent([[0, 0], [width, height]]); // <-- no clue what this does
 
   const zoom = d3.zoom()
-      .scaleExtent([1 << 8, 1 << 22])
-      .extent([[0, 0], [width, height]])
-      .on("zoom", (event) => zoomed(event.transform));
+    .scaleExtent([1 << 8, 1 << 22]) // <-- scale extents. if you make the upper limit too high, e.g. 1<<28, then zooming in really deep gets "jittery". feels like, theoretically, there should be a way to "reset" the zoom after a certain depth to make in-zooming effectively infinite. but i probably don't need that capability for this project anyway.
+    .extent([[0, 0], [width, height]]) // <-- no clue what this does
+    .on("zoom", (event) => zoomed(event.transform));
 
   const tileGroup = svg.append("g")
       .attr("pointer-events", "none")
@@ -50,7 +51,7 @@ const map = function() {
       .call(zoom)
       .call(zoom.transform, d3.zoomIdentity
         .translate(width >> 1, height >> 1)
-        .scale(1 << 10));
+        .scale(1 << 8)); // <-- initial scale
 
   function zoomed(transform) {
     // console.log(transform)
@@ -72,22 +73,40 @@ const map = function() {
 
     tile = tile.data(tiles, d => d).join(
       enter => enter.append("g")
-          .attr("transform", ([x, y]) => `translate(${x}, ${y}) scale(${1 / 256})`)
-          .call(g => g.append("rect")
-              .attr("fill", d => d3.interpolateRainbow(hilbert(...d)))
-              .attr("fill-opacity", 0.5)
-              .attr("stroke", "black")
-              .attr("width", 256)
-              .attr("height", 256))
-          .call(g => g.append("text")
-              .attr("x", "0.4em")
-              .attr("y", "1.2em")
-              .text(d => d.join("/")))
+        .attr("transform", ([x, y]) => `translate(${x}, ${y}) scale(${1 / 256})`)
+        .call(g => g.append("rect")
+          .attr("fill", d => d3.interpolateRainbow(hilbert(...d)))
+          .attr("fill-opacity", 0.5)
+          .attr("stroke", "black")
+          .attr("width", 256)
+          .attr("height", 256))
+        .call(g => g.append("text")
+          .attr("x", "0.4em")
+          .attr("y", "1.2em")
+          .text(d => d.join("/")))
+        .call(g => g.append(d => circlesForDepth(d[2])))
     );
   }
 
+  // programatic scale, animates to the resulting zoom
+  // svg.transition().call(zoom.scaleBy, 20)
   return svg.node();
 }
+
+// TODO: right now this increases num of circles as you zoom in. actually want it to decrease that number.
+// and to position them where they came from in the "sub-tiles" that made up this tile
+function circlesForDepth(depth) {
+  const group = d3.create('svg:g')
+  const spacing = 256 / (depth + 2)
+  for (let i = 0; i <= depth; i++) {
+    group.append("circle")
+      .attr("cx", 128)
+      .attr("cy", (i+1)*spacing)
+      .attr("r", 100 * (depth+1) / MAX_DEPTH)
+  }
+  return group.node()
+}
+
 
 function hilbert(x, y, z) {
   let n = 1 << z, rx, ry, s, d = 0;
